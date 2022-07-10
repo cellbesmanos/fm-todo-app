25c2396ad3b9215db85af003c5e58b3c8f960de8
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-10 16:18:00 +0800

    Fix: Move render list logic to TodoList component

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,40 @@
 <template>
-  <div>This is the TodoList component</div>
+  <div>
+    <TodoItemList :tasks="sampleData" />
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import TodoItemList from "./TodoItemList.vue";
+
+const sampleData = [
+  {
+    id: 1,
+    task: "Write a cool JS library",
+  },
+  {
+    id: 2,
+    task: "Make it generic enough",
+  },
+  {
+    id: 3,
+    task: "Write README",
+  },
+  {
+    id: 4,
+    task: "Create some examples",
+  },
+  {
+    id: 5,
+    task: "Spam in Twitter and IRC to promote it",
+  },
+  {
+    id: 6,
+    task: "???",
+  },
+  {
+    id: 7,
+    task: "PROFIT",
+  },
+];
+</script>
