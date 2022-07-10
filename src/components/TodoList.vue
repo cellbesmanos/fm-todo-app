036a90449efe679a4e16409d8a92b01b5c5a1a24
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-10 16:35:27 +0800

    Feat: Add new tasks on submit

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>
-    <TodoItemList :tasks="sampleData" />
-  </div>
+  <form @submit.prevent="addNewTask">
+    <input
+      v-model.trim="userInput"
+      type="text"
+      placeholder="Feed the dogs..."
+    />
+
+    <button type="submit">Add Task</button>
+  </form>
+
+  <TodoItemList :tasks="reversedList" />
 </template>
 
 <script setup>
+import { ref, computed } from "vue";
 import TodoItemList from "./TodoItemList.vue";
 
-const sampleData = [
+const userInput = ref("");
+const sampleData = ref([
   {
     id: 1,
     task: "Write a cool JS library",
@@ -36,5 +46,15 @@ const sampleData = [
     id: 7,
     task: "PROFIT",
   },
-];
+]);
+const itemsCount = computed(() => sampleData.value.length);
+const reversedList = computed(() => [...sampleData.value].reverse());
+
+function addNewTask() {
+  sampleData.value.push({
+    id: itemsCount + 1,
+    task: userInput.value,
+  });
+  userInput.value = "";
+}
 </script>
