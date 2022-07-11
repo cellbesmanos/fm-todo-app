18016c9cf67a56aa33623007efc9492f118346f2
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-11 11:51:50 +0800

    Feat: Add TodoListTollbar component

---
+++ src/components/TodoListToolbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>7 tasks remaining</p>
+
+    <div>
+      <button type="button" data-filter="all">All</button>
+      <button type="button" data-filter="active">Active</button>
+      <button type="button" data-filter="completed">Completed</button>
+    </div>
+
+    <button type="button">Clear Completed</button>
+  </div>
+</template>
+
+<script setup></script>
