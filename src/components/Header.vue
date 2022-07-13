0419c827eaeff3f8fa6cf94dd96ad2db2982a8ca
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-13 13:42:18 +0800

    Feat: Add Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="header">
+    <div class="header__toolbar">
+      <h1>Todo</h1>
+    </div>
+  </div>
+</template>
+
+<style>
+.header {
+  --height: 20rem;
+  min-height: var(--height);
+  background-image: url("../assets/bg-mobile-light.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+}
+
+@media screen and (min-width: 34.375em) {
+  .header {
+    --height: 30rem;
+    background-image: url("../assets/bg-desktop-light.jpg");
+  }
+}
+
+.header__toolbar {
+  padding-block-start: 4.8rem;
+  padding-inline: 2.4rem;
+  margin-inline: auto;
+  width: min(100%, 54rem);
+}
+
+.header__toolbar h1 {
+  color: white;
+  font-weight: 700;
+  font-size: 28px;
+  text-transform: uppercase;
+  letter-spacing: 0.33em;
+}
+</style>
