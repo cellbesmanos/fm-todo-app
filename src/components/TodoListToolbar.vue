63ad395a0215dacfc34623f949fcbf39d0c4f734
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-13 10:03:33 +0800

    Feat: Style TodoListToolbar

--- src/components/TodoListToolbar.vue
+++ src/components/TodoListToolbar.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
+  <div class="todoList__toolbar">
     <p>
       {{ `${remainingTasks} ${remainingTasks > 1 ? "tasks" : "task"} left` }}
     </p>
 
-    <div>
+    <div class="todoList__toolbar-filters">
       <button
         v-for="{ id, filter, label } in filters"
         @click="handleClick('filter', $event)"
@@ -62,8 +62,55 @@ function handleClick(type, event = null) {
 </script>
 
 <style>
+.todoList__toolbar {
+  --padding: 2.4rem;
+
+  position: relative;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  min-height: 5rem;
+  padding-inline: var(--padding);
+
+  font-size: 1.2rem;
+  font-weight: var(--fw-400);
+
+  border-radius: var(--bradius);
+  background-color: white;
+  box-shadow: 0 0.2rem 0.8rem hsl(236, 33%, 92%);
+}
+
+.todoList__toolbar button {
+  padding-block-start: 0.4rem;
+  font-size: inherit;
+}
+
+.todoList__toolbar-filters {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1.6rem;
+}
+
+@media screen and (max-width: 31.25em) {
+  .todoList__toolbar-filters {
+    position: absolute;
+    top: calc(4.8rem + 1.6rem);
+    left: 0;
+    width: 100%;
+    min-height: 4.8rem;
+    border-radius: inherit;
+    background-color: inherit;
+    box-shadow: inherit;
+  }
+}
+
+.todoList__toolbar-filters button {
+  font-weight: var(--fw-500);
+}
+
 .todoList__filter--active {
-  border: 1px solid hsl(120, 54%, 33%);
-  background-color: transparent;
+  color: var(--clr-blue);
 }
 </style>
